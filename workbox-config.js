dd47e8bccd5eaa13b9caac6340077ce646e2a860
chrisdaaz <chris-diaz@northwestern.edu>
2020-03-18 14:19:45 -0500

    update service workers

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   "globDirectory": "_site/",
   "globPatterns": [
-    "**/*.{html,png,js,css,scss,md,eot,ttf,woff,json,xml,ico,jpg,txt,svg}"
+    "**/*.{html,png,js,css,json,xml,ico,jpg,JPG,txt,svg}"
   ],
-  "swDest": "sw.js",
-  "swSrc": "service-worker.js"
+  "swDest": "sw.js"
 };
